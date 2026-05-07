59d42db50c096e59861ddec2ef70b6f241bbd3b2
Aaron Davis <arndvs@gmail.com>
2026-05-07 11:08:31 -0700

    chore: suppress hydration warning on html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${fontDisplay.variable} ${fontSans.variable} ${fontMono.variable}`}
+      suppressHydrationWarning
     >
       <body>
         {children}
